a784388732961c230f23b62cbc022c47d40f5841
pqthinh <thinhpq@its-global.vn>
2021-11-06 00:50:06 +0700

    doc api

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -1,28 +1,103 @@
-module.exports = app => {
+module.exports = (app) => {
   const tutorials = require("../controllers/tutorial.controller.js");
 
   var router = require("express").Router();
 
   // Create a new Tutorial
+  /**
+   * @swagger
+   * /api/tutorials:
+   *   post:
+   *     description: create a new tutorial
+   *     parameters:
+   *      - name: title
+   *        description: title of the book
+   *        in: formData
+   *        required: true
+   *        type: string
+   *     responses:
+   *       201:
+   *         description: Created
+   */
   router.post("/", tutorials.create);
 
   // Retrieve all Tutorials
+  /**
+   * @swagger
+   * /api/tutorials:
+   *   get:
+   *     description: Get all tutorial
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.get("/", tutorials.findAll);
 
   // Retrieve all published Tutorials
+  /**
+   * @swagger
+   * /api/tutorials/published:
+   *   get:
+   *     description:  Retrieve all published Tutorials
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.get("/published", tutorials.findAllPublished);
 
   // Retrieve a single Tutorial with id
+  /**
+   * @swagger
+   * /api/tutorials/:id:
+   *   get:
+   *     description:  Retrieve a single Tutorial with id
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.get("/:id", tutorials.findOne);
 
   // Update a Tutorial with id
+  /**
+   * @swagger
+   * /api/tutorials/:id:
+   *   put:
+   *     description:  Update a Tutorial with id
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.put("/:id", tutorials.update);
 
   // Delete a Tutorial with id
+  /**
+   * @swagger
+   * /api/tutorials/:id:
+   *   delete:
+   *     description:  Delete a Tutorial with id
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.delete("/:id", tutorials.delete);
 
   // Delete all Tutorials
+  /**
+   * @swagger
+   * /api/tutorials/:
+   *   delete:
+   *     description:  Delete all Tutorials
+   *     parameters:
+   *     responses:
+   *       200:
+   *         description: success
+   */
   router.delete("/", tutorials.deleteAll);
 
-  app.use('/api/tutorials', router);
+  app.use("/api/tutorials", router);
 };
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 var corsOptions = {
   origin: "*",
@@ -14,64 +14,46 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
+  swaggerDefinition: {
     info: {
-      title: "LogRocket Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "LogRocket",
-        url: "https://logrocket.com",
-        email: "info@email.com",
-      },
+      title: "Library API",
+      version: "1.0.0",
     },
-    servers: [
-      {
-        url: "http://localhost:3000/books",
-      },
-    ],
   },
-  apis: ["./routes/books.js"],
+  apis: ["server.js", "app/routes/turorial.routes.js"],
 };
 
 const specs = swaggerJsdoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 const db = require("./app/models");
 
 db.sequelize.sync();
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
 
-// simple route
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Get all info app
+ *     responses:
+ *       200:
+ *         description: Success
+ *
+ */
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to wemaket application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
-
-//initialize the app.
-async function initialize(){    
+async function initialize() {
   app.listen(PORT);
-};
+}
 
-initialize()
-  .finally(
-      () => console.log(`app started on port:${PORT}`)
-  );
+initialize().finally(() => console.log(`app started on port:${PORT}`));
