92fb985c3d1b66c4b3b70b5ea69c68f717921575
pqthinh <thinhpq@its-global.vn>
2021-11-07 05:50:44 +0700

    paginate product table

--- api/mysql/product.js
+++ api/mysql/product.js
@@ -2,15 +2,25 @@ const dbs = require("./dbs");
 
 const Product = {
   getAllPostActive: async (req, res, next) => {
-    let conn;
+    let conn,
+      { limit = 10, offset = 0 } = req.query;
     try {
       conn = await dbs.getConnection();
       await conn.beginTransaction();
       let sql, result;
-      sql = `select product.*, user.name, user.address, user.email, user.phone, user.avatar from user, product where product.status ="active" `;
-      result = await conn.query(sql);
+      sql = `select product.*, user.username, user.address, user.email, user.phone, user.avatar from user, product where product.status ="active" limit ? offset ?`;
+      result = await conn.query(sql, [
+        Number(limit),
+        Number(offset > 0 ? offset : 0) * Number(limit),
+      ]);
+      const total = await conn.query("select count(*) as total from product");
       await conn.commit();
-      res.json(result[0]);
+      const response = {
+        total: total[0][0].total,
+        page: Number(offset) + 1,
+        result: result[0],
+      };
+      res.json(response);
     } catch (err) {
       await conn.rollback();
       next(err);
