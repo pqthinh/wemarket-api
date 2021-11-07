953b2375d1d655b4ed803b2b4a2ba1b085678b22
pqthinh <thinhpq@its-global.vn>
2021-11-07 14:27:01 +0700

    api get product detail

--- api/mysql/product.js
+++ api/mysql/product.js
@@ -15,11 +15,75 @@ const Product = {
       ]);
       const total = await conn.query("select count(*) as total from product");
       await conn.commit();
+
       const response = {
         total: total[0][0].total,
         page: Number(offset) + 1,
         result: result[0],
       };
+      res.json(response);
+    } catch (err) {
+      await conn.rollback();
+      next(err);
+    } finally {
+      await conn.release();
+    }
+  },
+  getProductDetail: async (req, res, next) => {
+    let conn;
+    let { idProduct } = req.body;
+    try {
+      conn = await dbs.getConnection();
+      await conn.beginTransaction();
+      let sql, result;
+      sql = `select product.*, user.* from user, product where product.id = ? and user.id = product.userId and product.status ="active"`;
+
+      result = await conn.query(sql, [idProduct.toString()]);
+      images = await conn.query("select * from image where productId=?", [
+        idProduct,
+      ]);
+
+      await conn.commit();
+
+      const data = {
+        ...result[0][0],
+        images: images[0],
+      };
+      const response = {
+        message: "success",
+        status: 1,
+        result: data,
+      };
+      res.json(response);
+    } catch (err) {
+      await conn.rollback();
+      next(err);
+    } finally {
+      await conn.release();
+    }
+  },
+
+  adminGetAllPost: async (req, res, next) => {
+    let conn,
+      { limit = 10, offset = 0 } = req.query;
+    try {
+      conn = await dbs.getConnection();
+      await conn.beginTransaction();
+      let sql, result;
+      sql = `select product.*, user.username, user.address, user.email, user.phone, user.avatar from user, product limit ? offset ?`;
+      result = await conn.query(sql, [
+        Number(limit),
+        Number(offset > 0 ? offset : 0) * Number(limit),
+      ]);
+      const total = await conn.query("select count(*) as total from product");
+      await conn.commit();
+
+      const response = {
+        total: total[0][0].total,
+        page: Number(offset) + 1,
+        result: result[0],
+      };
+
       res.json(response);
     } catch (err) {
       await conn.rollback();
